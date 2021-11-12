45ae08feb1e29b164a8b1292b8925c7a899c4ca9
arridepp <74824751+arridepp@users.noreply.github.com>
2021-11-12 19:55:40 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <footer className="flex justify-center">
-            <strong className="text-white text-2xl">© Copyright ARRi Depp</strong>
-        </footer>
-    );
-}
